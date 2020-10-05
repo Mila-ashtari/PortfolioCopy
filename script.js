ea7519b107e83e80c4b692feb5cf84d9bc2a5992
Mila <mashtari1991@gmail.com>
2020-10-04 20:49:57 -0400

    projects descriptions chenged

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 const App={}
+App.mainMenuDisplay=function(){
+    $(window).scroll(function () {
+        let scroll = $(window).scrollTop();
+        let displayLimit=$('#aboutMe').offset().top - 80
+        if(scroll> displayLimit){
+            $('nav.mainMenu').css('display','flex')
+        }
+        else{
+            $('nav.mainMenu').css('display','none')
+        }
+    });
+}
 App.menu=function(){
     $('.hamburger--spin').on('click', function(){
         $('.hamburger--spin').toggleClass('is-active')
@@ -16,6 +28,7 @@ App.smoothScroll=function(){
 App.inint=function(){
     App.menu()
     App.smoothScroll()
+    App.mainMenuDisplay()
 }
 $(function(){
     App.inint()
